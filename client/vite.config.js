46e5c316e356e8a2a3a0c9485a89d4484d7b4b13
Jerast <jose.romero.9602@outlook.com>
2024-08-02 22:48:05 -0500

    chore(client): add jsconfig.json for simplified paths with Vite

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+		alias: {
+			'@app': resolve(__dirname, './src/app'),
+		},
+	},
 })
