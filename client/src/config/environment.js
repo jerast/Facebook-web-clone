13f985ce155d18b271065cee0f8268fc7f2179ad
Jerast <jose.romero.9602@outlook.com>
2024-08-02 22:33:04 -0500

    chore: set environment variables files for client and server

---
+++ client/src/config/environment.js
@@ -0,0 +1,2 @@
+export const API_PATH = import.meta.env.VITE_FACEBOOK_WEB_CLONE_API_PATH ?? 'http://localhost:1234'
+export const SOCKET_PATH = import.meta.env.VITE_FACEBOOK_WEB_CLONE_SOCKET_PATH ?? 'http://localhost:1234'
