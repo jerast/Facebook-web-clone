3a3510110496a2889c697e0e81089661114006b0
Jerast <jose.romero.9602@outlook.com>
2024-08-08 00:36:22 -0500

    fix(server): import dotenv to properly load environment variables

--- server/src/config/environment.js
+++ server/src/config/environment.js
@@ -1,7 +1,10 @@
+import dotenv from 'dotenv'
 
-export const PORT = process.env.FACEBOOK_WEB_CLONE_PORT || 8172
-export const DB_URI = process.env.FACEBOOK_WEB_CLONE_DB_URI || "mongodb://localhost:27017/socket-chat"
+dotenv.config()
+
+export const PORT = process.env.FACEBOOK_WEB_CLONE_PORT || 1234
+export const DB_URI = process.env.FACEBOOK_WEB_CLONE_DB_URI || "mongodb://url_database"
 export const JWT_SEED = process.env.FACEBOOK_WEB_CLONE_JWT_SEED || "seed"
-export const CLOUDINARY_CLOUD = process.env.FACEBOOK_WEB_CLONE_CLOUDINARY_CLOUD || "dlgvigwlh"
-export const CLOUDINARY_KEY = process.env.FACEBOOK_WEB_CLONE_CLOUDINARY_KEY || 572614815284646
-export const CLOUDINARY_SECRET = process.env.FACEBOOK_WEB_CLONE_CLOUDINARY_SECRET || "OiEnAJtq05OINFOUs9rsIH_6mFk"
+export const CLOUDINARY_CLOUD = process.env.FACEBOOK_WEB_CLONE_CLOUDINARY_CLOUD || "abc"
+export const CLOUDINARY_KEY = process.env.FACEBOOK_WEB_CLONE_CLOUDINARY_KEY || 12345678
+export const CLOUDINARY_SECRET = process.env.FACEBOOK_WEB_CLONE_CLOUDINARY_SECRET || "cloud_secret"
