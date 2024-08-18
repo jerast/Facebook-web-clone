583471ddee36dcaa417bfe36eca81c11253825b6
Jerast <jose.romero.9602@outlook.com>
2024-08-18 01:37:40 -0500

    feat(client): complete apps sidebar in main layout

--- client/src/app/config/uiConstants.js
+++ client/src/app/config/uiConstants.js
@@ -2,3 +2,25 @@ export const uiFooter = {
   languages: ["English (US)", "Español", "Français (France)", "Português (Brasil)", "Italiano", "Deutsch", "العربية", "हिन्दी", "中文(简体)", "日本語"],
   links: ["Sign Up", "Log In", "Messenger", "Facebook Lite", "Video", "Places", "Games", "Marketplace", "Meta Pay", "Meta Store", "Meta Quest", "Meta AI", "Instagram", "Threads", "Fundraisers", "Services", "Voting Information Center", "Privacy Policy", "Privacy Center", "Groups", "About", "Create ad", "Create Page", "Developers", "Careers", "Cookies", "Ad choices", "Terms", "Help", "Contact Uploading & Non-Users"],
 }
+
+export const appsSidebarItems = [
+  { name: 'Friends', offset: 'object-[0px_-0px]' },
+  { name: 'Memories', offset: 'object-[0px_-37px]' },
+  { name: 'Saved', offset: 'object-[0px_-74px]' },
+  { name: 'Groups', offset: 'object-[0px_-111px]' },
+  { name: 'Video', offset: 'object-[0px_-148px]' },
+  { name: 'Marketplace', offset: 'object-[0px_-185px]' },
+  { name: 'Feeds', offset: 'object-[0px_-222px]' },
+  { name: 'Events', offset: 'object-[0px_-259px]' },
+  { name: 'Ads Manager', offset: 'object-[0px_-296px]' },
+  { name: 'Birthdays', offset: 'object-[0px_-333px]' },
+  { name: 'Climate Science Center', offset: 'object-[0px_-370px]' },
+  { name: 'Fundraisers', offset: 'object-[0px_-407px]' },
+  { name: 'Gaming Video', offset: 'object-[0px_-444px]' },
+  { name: 'Messenger', offset: 'object-[0px_-481px]' },
+  { name: 'Messenger Kids', offset: 'object-[0px_-518px]' },
+  { name: 'Orders and payments', offset: 'object-[0px_-555px]' },
+  { name: 'Pages', offset: 'object-[0px_-592px]' },
+  { name: 'Play games', offset: 'object-[0px_-629px]' },
+  { name: 'Recent ad activity', offset: 'object-[0px_-666px]' },
+]
