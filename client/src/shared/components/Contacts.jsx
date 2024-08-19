2c80f74958beaa0fd860859936a7adcab66f3554
Jerast <jose.romero.9602@outlook.com>
2024-08-18 20:17:43 -0500

    feat(client): complete design of contacts sidebar

--- client/src/shared/components/Contacts.jsx
+++ client/src/shared/components/Contacts.jsx
@@ -1,7 +1,68 @@
+import { useAuthStore } from '@app/store/auth.store'
+import { image } from '@shared/utils/img'
+import OptionsIcon from '@app/assets/svg/options.svg?react'
+
 export const Contacts = () => {
+  const { user } = useAuthStore()
+    
   return (
-    <section className="contacts">
-      <h1>Contacts</h1>
-    </section>
+    <aside className="contacts">
+      <section className="contacts__group">
+        <div className="contacts__header">
+          <h3 className="contacts__header-text">Contacts</h3>
+          <button className="contacts__header-button">
+            <OptionsIcon className="contacts__header-button-icon" />
+          </button>
+        </div>
+        <ul className="contacts-list">
+          <li className="contacts-item contacts-item--online">
+            <img className="contacts-item__profile-picture" src={image(user.profile, 40)} alt="" />
+            <span className="contacts-item__username">User name</span>
+          </li>
+          <li className="contacts-item contacts-item--online">
+            <img className="contacts-item__profile-picture" src={image(user.profile, 40)} alt="" />
+            <span className="contacts-item__username">User name</span>
+          </li>
+          <li className="contacts-item contacts-item--online">
+            <img className="contacts-item__profile-picture" src={image(user.profile, 40)} alt="" />
+            <span className="contacts-item__username">User name</span>
+          </li>
+          <li className="contacts-item">
+            <img className="contacts-item__profile-picture" src={image(user.profile, 40)} alt="" />
+            <span className="contacts-item__username">User name</span>
+          </li>
+          <li className="contacts-item">
+            <img className="contacts-item__profile-picture" src={image(user.profile, 40)} alt="" />
+            <span className="contacts-item__username">User name</span>
+          </li>
+          <li className="contacts-item">
+            <img className="contacts-item__profile-picture" src={image(user.profile, 40)} alt="" />
+            <span className="contacts-item__username">User name</span>
+          </li>
+          <li className="contacts-item">
+            <img className="contacts-item__profile-picture" src={image(user.profile, 40)} alt="" />
+            <span className="contacts-item__username">User name</span>
+          </li>
+        </ul>
+      </section>
+
+      <hr className="sidebar__hr" />
+
+      <section className="contacts__group">
+        <div className="contacts__header">
+          <h3 className="contacts__header-text">Group conversations</h3>
+        </div>
+        <ul className="contacts-list">
+          <li className="contacts-item">
+            <img className="contacts-item__profile-picture" src="/img/profile.jpg" alt="" />
+            <span className="contacts-item__username">Group chat</span>
+          </li>
+          <li className="contacts-item">
+            <img className="contacts-item__profile-picture" src="/img/profile.jpg" alt="" />
+            <span className="contacts-item__username">Group chat</span>
+          </li>
+        </ul>
+      </section>
+    </aside>
   )
 }
